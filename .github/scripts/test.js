84b59a5d58ac35447eb5d6cd83cb006a15ef35c7
robinatwork1999 <103563730+robinatwork1999@users.noreply.github.com>
2023-05-18 01:19:18 +0530

    Update test.js

--- .github/scripts/test.js
+++ .github/scripts/test.js
@@ -1,2 +1,9 @@
 console.log(process.argv)
 console.log(process.argv[2])
+const prBody = process.argv[2];
+const sectionCheckArr = ["## Description", "## Implemention Details", "## Link to Jira Ticket"];
+const seperator = "***";
+
+sectionCheckArr.forEach((x,i) => {
+  const indexx = prBody.indexOf(x);
+})
